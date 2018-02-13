2e342e20a0442ac4e60dd9bb8ed899a13d99d0b6
amelafili <31477792+amelafili@users.noreply.github.com>
2018-02-13 16:40:35 -0500

    added button classes and found them on javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+var buttonElement = document.getElementById('button-kick')
+  buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
+
+var buttonElement = document.getElementById('button-clap')
+  buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
