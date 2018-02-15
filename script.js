3c69be4ce0e427524f9970d515076cef702f3184
amelafili <31477792+amelafili@users.noreply.github.com>
2018-02-15 18:28:09 -0500

    added background color and border around buttons

--- script.js
+++ script.js
@@ -4,14 +4,32 @@ buttonKick.addEventListener('click', function () {
   audioKick.play()
 })
 
+var audioKick = document.getElementById('audio-kick')
+var buttonKick = document.getElementById('button-kick')
+buttonKick.addEventListener('mouseenter', function () {
+  audioKick.play()
+})
+
 var audioClap = document.getElementById('audio-clap')
 var buttonClap = document.getElementById('button-clap')
   buttonClap.addEventListener('click', function () {
   audioClap.play()
 })
 
+var audioClap = document.getElementById('audio-clap')
+var buttonClap = document.getElementById('button-clap')
+  buttonClap.addEventListener('mouseenter', function () {
+  audioClap.play()
+})
+
 var audioBoom = document.getElementById('audio-boom')
 var buttonBoom = document.getElementById('button-boom')
 buttonBoom.addEventListener('click', function () {
+  audioBoom.play()
+})
+
+var audioBoom = document.getElementById('audio-boom')
+var buttonBoom = document.getElementById('button-boom')
+buttonBoom.addEventListener('mouseenter', function () {
   audioBoom.play()
 })
